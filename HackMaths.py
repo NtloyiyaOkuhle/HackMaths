d1e4f38edc79a43e1057e07e7a890589f466b987
Xh0s4rH4ck3r <blessing.ntloyiya@gmail.com>
2023-03-14 12:11:23 +0200

    Add files via upload

--- HackMaths.py
+++ HackMaths.py
@@ -1,154 +1,117 @@
-import math
 import sympy
-import pint
+from sympy import symbols, Eq, solve, Integral, Derivative, Matrix, lambdify
+from sympy.plotting import plot
 
-# Create an empty dictionary to store previously solved problems
+# Dictionary to store previously solved problems
 prev_problems = {}
 
-# Create a dictionary to store the variables and their values
-variables = {}
+#Define function for solving problems
+def solve_problem(problem_str):
+    # Check if the problem is an equation
+    if "=" in problem_str:
+        # Parse the equation
+        left, right = problem_str.split("=")
+        expr = sympy.parsing.sympy_parser.parse_expr(left) - sympy.parsing.sympy_parser.parse_expr(right)
 
-# Create a UnitRegistry object from pint
-ureg = pint.UnitRegistry()
+        # Solve the equation
+        solutions = solve(expr, symbols('x'))
 
-# Define the available functions
-functions = {
-    'sin': sympy.sin,
-    'cos': sympy.cos,
-    'tan': sympy.tan,
-    'log': sympy.log,
-    'exp': sympy.exp,
-    'sqrt': sympy.sqrt,
-    'abs': sympy.Abs,
-    'factorial': sympy.factorial,
-    'integrate': sympy.integrate,
-    'diff': sympy.diff,
-    'solve': sympy.solve,
-    'simplify': sympy.simplify,
-    'expand': sympy.expand,
-    'factor': sympy.factor,
-    'cancel': sympy.cancel,
-}
+    # Check if the problem is a system of equations
+    elif 'system' in problem_str:
+        eqns = problem_str.split(',')
+        eqns = [sympy.parsing.sympy_parser.parse_expr(eqn) for eqn in eqns]
+        vars = []
+        for eqn in eqns:
+            vars += list(eqn.free_symbols)
+        vars = sorted(set(vars), key=lambda var: str(var))
+        solutions = sympy.solve(eqns, vars)
 
-# Define a function to evaluate a mathematical expression
-def evaluate(expression):
-    # Replace variables with their values
-    for var, val in variables.items():
-        expression = expression.replace(var, str(val))
+    # Check if the problem is a polynomial equation
+    elif 'polynomial' in problem_str:
+        expr = sympy.parsing.sympy_parser.parse_expr(problem_str.replace('polynomial', ''))
+        solutions = sympy.roots(expr, multiple=True)
 
-    # Parse the expression with sympy
-    expr = sympy.sympify(expression)
-
-    # Evaluate the expression with units conversion
-    try:
-        # Try to evaluate the argument as a number with units
-        arg = ureg(arg_str)
-        if arg.dimensionality == pint.dimensionless.dimensionality:
-            # The argument is a pure number, without units
-            arg = float(arg)
-        else:
-            # The argument has units, convert to base units
-            arg = arg.to_base_units().magnitude
-    except pint.UndefinedUnitError:
-        # If the argument is not a number with units or a variable, raise an error
-        raise ValueError(f'Invalid argument: {arg_str}')
-
-
-    return result, unit
-
-# Define a function to format a number with a specified number of decimal places
-def format_number(number, decimals):
-    if decimals is None:
-        return str(number)
     else:
-        return f'{number:.{decimals}f}'
+        # Parse the problem into a SymPy expression
+        expr = sympy.parsing.sympy_parser.parse_expr(problem_str)
 
-# Define a function to display the list of available functions
-def display_functions():
-    print('Available functions:')
-    for func in sorted(functions.keys()):
-        print(f'- {func}(...)')
+        # Check the type of the problem and solve accordingly
+        if isinstance(expr, Eq):
+            # Solve equation
+            solutions = solve(expr, symbols('x'))
+        elif isinstance(expr, Integral):
+            # Solve integral
+            solutions = sympy.integrate(expr, symbols('x'))
+        elif isinstance(expr, Derivative):
+            # Solve derivative
+            solutions = sympy.diff(expr, symbols('x'))
+        elif isinstance(expr, Matrix):
+            # Solve matrix operations
+            solutions = expr.doit()
+        else:
+            # Solve generic problem
+            solutions = sympy.simplify(expr)
 
-# Define a function to display the history of solved problems
-def display_history():
-    print('History of solved problems:')
-    for problem, result in prev_problems.items():
-        print(f'{problem} = {format_number(result[0], result[1])}')
+    # Check for additional functionality
+    if 'sin' in problem_str or 'cos' in problem_str:
+        solutions = sympy.solveset(expr, symbols('x'))
+    elif 'tan' in problem_str:
+        expr = sympy.parsing.sympy_parser.parse_expr(problem_str.replace('tan', 'sympy.tan'))
+        solutions = expr.evalf()
+    elif 'limit' in problem_str:
+        expr, x, a = problem_str.split(',')
+        expr = sympy.parsing.sympy_parser.parse_expr(expr)
+        a = float(a)
+        solutions = sympy.limit(expr, symbols(x), a)
+    elif 'second derivative' in problem_str:
+        expr = problem_str.replace('second derivative of ', '')
+        expr = sympy.parsing.sympy_parser.parse_expr(expr)
+        solutions = sympy.diff(expr, symbols('x'), 2)
+    elif 'heat equation' in problem_str:
+        u = sympy.Function('u')(symbols('x'), symbols('y'))
+        eq = sympy.Eq(sympy.diff(u, symbols('x'), symbols('x')) + sympy.diff(u, symbols('y'), symbols('y')), 0)
+        solutions = sympy.pde.pdsolve(eq)
+    elif 'plot' in problem_str:
+        # Parse the function and plot
+        expr = sympy.parsing.sympy_parser.parse_expr(problem_str.replace('plot ', ''))
+        p = plot(expr, show=False)
+        p.show()
+        return "Graph displayed."
+    elif 'numeric' in problem_str:
+        # Parse the function, convert to a lambda function, and evaluate numerically
+        expr = sympy.parsing.sympy_parser.parse_expr(problem_str.replace('numeric ', ''))
+        f = lambdify(x, expr)
+        a, b = [float(x) for x in input("Enter the start and end points: ").split()]
+        n = int(input("Enter the number of steps: "))
+        x_vals = [a + i*(b-a)/n for i in range(n+1)]
+        y_vals = [f(x) for x in x_vals]
+        p = plot(expr, (x, a, b), show=False)
+        p.scatter(x_vals, y_vals)
+        p.show()
+        return "Graph displayed."
+    else:
+        pass
 
-# Define a function to display the options menu
-def display_options():
-    print('Options:')
-    print('- Set the number of decimal places to display (e.g., decimals=2)')
-    print('- Display the list of available functions (type "functions")')
-    print('- Display the history of solved problems (type "history")')
-    print('- Exit the program (type "exit")')
 
+    # Store the solution in the previous problems dictionary
+    prev_problems[problem_str] = solutions
+
+    # Return the solution(s) to the user
+    return solutions
+
+# Main loop
 while True:
     # Get user input
-    user_input = input('Enter a mathematical problem to solve (or "exit" to quit): ')
-
-    # Check if the user wants to exit
-    if user_input.lower() == 'exit':
-        break
-
-    # Check if the user wants to display the list of available functions
-    if user_input.lower() == 'functions':
-        display_functions()
-        continue
-
-    # Check if the user wants to display the history of solved problems
-    if user_input.lower() == 'history':
-        display_history()
-        continue
-        
-    # Check if the user wants to set the number of decimal places to display
-    if 'decimals=' in user_input:
-        try:
-            decimals = int(user_input.split('=')[1])
-        except ValueError:
-            raise ValueError('Invalid input: decimals must be an integer')
-        continue
-    else:
-        decimals = None
-
+    user_input = input("Enter a mathematical problem: ")
 
     # Check if the problem has already been solved
     if user_input in prev_problems:
-        result, unit = prev_problems[user_input]
-        print(f'Result (from history): {format_number(result, decimals)} {unit}')
+        print(f"Result (from history): {prev_problems[user_input]}")
         continue
 
-    # Split the problem into function and argument(s)
-    if '(' not in user_input:
-        raise ValueError('Invalid input: missing parentheses')
-    parts = user_input.split('(')
-    function_name = parts[0]
-    args_str = parts[1].strip(')')
-
-
-    # Parse the arguments
-    args = []
-    for arg_str in args_str.split(','):
-        arg_str = arg_str.strip()
-        # Check if the argument is a variable
-        if arg_str in variables:
-            arg = variables[arg_str]
-        else:
-            try:
-                # Try to evaluate the argument as a number with units
-                arg = ureg(arg_str).to_base_units().magnitude
-            except pint.UndefinedUnitError:
-                # If the argument is not a number with units or a variable, raise an error
-                raise ValueError(f'Invalid argument: {arg_str}')
-        args.append(arg)
-
-    # Call the function with the parsed arguments
-    if function_name not in functions:
-        raise ValueError(f'Invalid function name: {function_name}')
-    function = functions[function_name]
-    result, unit = function(*args)
-
-    # Store the result in the history dictionary
-    prev_problems[user_input] = (result, unit)
-    # Display the result
-    print(f'Result: {format_number(result, decimals)} {unit}')
+    # Try to solve the problem
+    try:
+        solutions = solve_problem(user_input)
+        print(f"Result: {solutions}")
+    except:
+        print("Error: Could not solve the problem.")
